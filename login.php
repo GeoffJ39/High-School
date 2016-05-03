c09c1e8bd63edc0f2935531337c0e464aa33e6bc
ROOSTERs <surinrao12@gmail.com>
2016-05-03 11:58:00 -0400

    fixed background and added a new tab

--- login.php
+++ login.php
@@ -7,10 +7,68 @@
             background-size: cover;
             background-repeat: no-repeat;
 
-            background-image: url(background.png);
+            background-image: url(background.jpg);
 
         }
     </style>
+    <nav class="navbar navbar-default navbar-fixed-top">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#"><span class="glyphicon glyphicon glyphicon-tint" aria-hidden="true"></span> Symbols</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="login.php">Login <span class="sr-only">(current)</span></a></li>
+                <li><a href="register.php">Register</a></li>
+                 <li><a href="team.php">Meet The Team</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">One more separated link</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <form class="navbar-form navbar-left" role="search">
+                <div class="input-group">
+                    <div class="input-group">
+                      <input type="text" class="form-control" placeholder="Search">
+                      <div class="input-group-btn">
+                      <input type="submit" class="btn btn-success">
+                      </div>
+                    </div>
+                </div>
+              </form>
+              <form class="navbar-form navbar-right" action="logout.php" method="post">
+                <button class="btn btn-warning">Sign out</button>
+              </form>
+              <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <?php
+                    require("common.php"); 
+                    $arr = array_values($_SESSION['user']);
+                    echo "<a href='#'>Welcome, " . $arr[1] . "</a>";
+                    ?>  
+                </li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+
 </body>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">

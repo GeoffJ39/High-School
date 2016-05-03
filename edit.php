5d083dee45183bc1edd322c2b2ffe861d55ba323
Le Garde Forestier Bleu <geoffrey.jiang@ucc.on.ca>
2016-05-03 11:38:36 -0400

    Update edit.php

--- edit.php
+++ edit.php
@@ -238,6 +238,57 @@
 	<!-- End of the second panel -->
 
 	<div class="alert alert-success" role="alert">Yay!</div>
+	
+	<?php
+		require("common.php");
+		$connection = mysql_connect($host, $username, $password) or die ("Unable to connect!");
+		mysql_select_db($dbname) or die ("Unable to select database!");
+		$search = mysql_escape_string($_POST['search']);
+		$query = "SELECT country FROM symbols WHERE animal LIKE '%$search%'";
+		$result = mysql_query($query) or die ("Error in query: $query. ".mysql_error());
+		if (mysql_num_rows($result) > 0) {
+    		// print them one after another
+    		echo "<table cellpadding=10 border=1>";
+    		while($row = mysql_fetch_row($result)) {
+        		echo "<tr>";
+				echo "<td>".$row[0]."</td>";
+        		//echo "<td>" . $row[1]."</td>";
+        		//echo "<td>".$row[2]."</td>";
+				//echo "<td><a href=".$_SERVER['PHP_SELF']."?id=".$row[0].">Delete</a></td>";
+        		echo "</tr>";
+    		}
+		    echo "</table>";
+		} else {
+
+    		// print status message
+    		echo "No rows found!";
+		}
+	?>
+
+
+	 <!-- This is the Search function -->
+    	<div class="panel panel-success">
+		<div class="panel-heading">
+			<h3 class="panel-title">Entry Submission</h3>
+		</div>
+		<div class="panel-body">
+	   	   <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+	   	<div class="grid">
+		    <div class="row">
+	  		<div class="col-lg-12">
+		    		<div class="input-group">
+				  <span class="input-group-addon" id="basic-addon1">Country</span>
+				  Search: <input type="text" name="search">
+			  	<div class="input-group-btn">
+			  		<input type="submit" name="submit">
+			  	</div>
+				</div>
+			</div>
+			</div>
+		</div>
+	    </form>
+		</div>
+		</div>
     
 	</body>
 </html>

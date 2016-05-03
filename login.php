55f0a93d652a0a1dafbd5aac6784e3f9180313cb
ROOSTERs <surinrao12@gmail.com>
2016-05-03 12:11:42 -0400

    added the balck navbar part

--- login.php
+++ login.php
@@ -11,9 +11,13 @@
 
         }
     </style>
+
     <nav class="navbar navbar-default navbar-fixed-top">
+       
           <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
+             <nav class="navbar navbar-inverse">
+     <img alt="Brand" src="http://www.how-to-draw-cartoons-online.com/image-files/how-to-draw-squidward.gif" width ="10">
             <div class="navbar-header">
               <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
@@ -23,6 +27,8 @@
               </button>
               <a class="navbar-brand" href="#"><span class="glyphicon glyphicon glyphicon-tint" aria-hidden="true"></span> Symbols</a>
             </div>
+            
+
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">

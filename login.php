b1525e2d0297cfd172fac6ce60f4a897b5a7b486
ROOSTERs <surinrao12@gmail.com>
2016-05-03 12:17:58 -0400

    added black nav

--- login.php
+++ login.php
@@ -17,7 +17,7 @@
           <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
              <nav class="navbar navbar-inverse">
-     <img alt="Brand" src="http://www.how-to-draw-cartoons-online.com/image-files/how-to-draw-squidward.gif" width ="10">
+     
             <div class="navbar-header">
               <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
@@ -25,8 +25,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#"><span class="glyphicon glyphicon glyphicon-tint" aria-hidden="true"></span> Symbols</a>
-            </div>
+            
             
 
 
